22071f2c14651bfa9452044c32c176fa1785256a
Jupa322 <94424176+Jupa322@users.noreply.github.com>
2021-11-16 17:13:07 -0800

    Update Grey forecasting.py

--- Grey forecasting.py
+++ Grey forecasting.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-class GrayForecast():
+class Grayprediction():
     def __init__(self, data, datacolumn=None):
 
         if isinstance(data, pd.core.frame.DataFrame):
@@ -20,17 +20,17 @@ class GrayForecast():
         else:
             self.data = pd.DataFrame(data, columns=['ElevationData'])
 
-        self.forecast_list = self.data.copy()
+        self.prediction_list = self.data.copy()
 
         if datacolumn:
             self.datacolumn = datacolumn
         else:
             self.datacolumn = None
 
-    def GMmakemodel(self, forecast=5):
-        if forecast > len(self.data):
+    def GMmakemodel(self, prediction=5):
+        if prediction > len(self.data):
             raise Exception
-        X_0 = np.array(self.forecast_list['ElevationData'].tail(forecast))
+        X_0 = np.array(self.prediction_list['ElevationData'].tail(prediction))
         X_1 = np.zeros(X_0.shape)
         for i in range(X_0.shape[0]):
             X_1[i] = np.sum(X_0[0:i+1])
@@ -51,23 +51,23 @@ class GrayForecast():
         def f(k):
             return (X_0[0]-b/a)*(1-np.exp(a))*np.exp(-a*(k))
 
-        self.forecast_list.loc[len(self.forecast_list)] = f(X_.shape[0])
+        self.prediction_list.loc[len(self.prediction_list)] = f(X_.shape[0])
 
-    def forecast(self, time=5, forecast_data_len=5):
+    def prediction(self, time=5, prediction_data_len=5):
         for i in range(time):
-            self.GMmakemodel(forecast=forecast_data_len)
+            self.GMmakemodel(prediction=prediction_data_len)
 
     def log(self):
-        res = self.forecast_list.copy()
+        res = self.prediction_list.copy()
         if self.datacolumn:
             res.columns = [self.datacolumn]
         return res
 
 
     def reset(self):
-        self.forecast_list = self.data.copy()
+        self.prediction_list = self.data.copy()
 
 
-gf = GrayForecast([12902678.34,12474061.06,11997350.86,11617700.42,11528992.9,11553500.51,11531363.09])
-gf.forecast(370)
-gf.log().to_csv('111forecast.csv')
+gf = Grayprediction([12902678.34,12474061.06,11997350.86,11617700.42,11528992.9,11553500.51,11531363.09])
+gf.prediction(370)
+gf.log().to_csv('finalresults.csv')
